ec91a96ad0b0cb02c81560f385aaf10add1175a6
Hack Reactor Students <accounts@hackreactor.com>
2016-09-30 17:19:30 -0700

    Implement click event for VideoListEntry to update VideoPlayer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,36 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={window.exampleVideoData[0]}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={window.exampleVideoData}/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      currentVideo: window.exampleVideoData[0],
+      videos: window.exampleVideoData
+    };
+  }
+
+  OnVideoListClick(event) {
+
+    var title = $(event.currentTarget).text();
+    //could fail for 2 videos with same name
+    var video = this.state.videos.filter(video => video.snippet.title === title)[0];
+    this.setState({
+      currentVideo: video
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.videos} videoListClick={this.OnVideoListClick.bind(this)}/>
+        </div>
+      </div>
+    );  
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
